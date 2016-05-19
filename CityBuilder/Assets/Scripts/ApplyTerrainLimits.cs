e67c34533e5687caef6c67d922a237060dd3af50
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2016-05-18 21:20:38 -0400

    Camera controls
    - Include RTS_Camera asset for basic camera controls
    - Remove some unrequired parts from basic import of RTS_Camera
    - Add script to limit panoramic position movement to terrain size
    - Setup basic components to start testing scene elements / camera controls
    - Change some keys to control camera
    - Adjust some values for scrolling speed. limits, initialization, etc.
    
    TODO:
    - Invert scrolling direction with mousewheel
    - Allow moving up/down with right-click (only left / right available currently)

---
+++ CityBuilder/Assets/Scripts/ApplyTerrainLimits.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using RTS_Cam;
+
+[RequireComponent(typeof(RTS_Camera))]
+public class ApplyTerrainLimits : MonoBehaviour {
+
+	public GameObject terrain;
+	private RTS_Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = gameObject.GetComponent<RTS_Camera>();	
+
+		// Camera limit set with ±limitX / limitY, so scaling has to be divided by 2
+		// Also, camera is moving in XY with Z downward, but terrain is oriented as XZ plane with Y upward
+		camera.limitX = terrain.transform.localScale.x / 2;
+		camera.limitY = terrain.transform.localScale.z / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

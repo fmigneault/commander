756beb282ec1b5944efea6492a0bd8aebfdfdd4f
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2016-05-22 02:39:31 -0400

    Initial combat unit models (opens issue #22, opens issue #9)
    - Adds modelisation of "Abrams" tank
    - Import "DragonLance" tank, modelisation to complete
    - Adds cannon turret movement support (forward for non-target, toward target when specified)
    
    Extra :
    
    - Remove most unrequired cubes on scene and obsolete code
    - Reorganize most of the folder structure according to asset relevance

---
+++ Assets/Scripts/Units/CombatUnitManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Units
+{
+	public class CombatUnitManager : MonoBehaviour 
+	{
+		public float MovingSpeed;
+
+
+		// Use this for initialization
+		void Start () 
+		{
+		
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+		
+		}
+	}
+}
---
+++ Assets/Scripts/Units/TankManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Units 
+{	
+	public class TankManager : MonoBehaviour
+	{		
+		public GameObject LookAtTarget;		// Specify a location to make the cannon look at (ennemy, building, default: forward if null)
+		public GameObject CannonTurret;		// Reference to cannon section of the tank
+		public GameObject CannonBarrel;		// Reference to barrel section of the tank (shooting)
+		public float CannonRotateSpeed;		// Cannon rotation speed in deg/s
+		public float CannonAttackRange;		// Minimum distance required to allow attacking
+
+		void Awake ()
+		{
+
+		}
+		
+
+		void Update ()
+		{				
+			transform.RotateAround(transform.position, transform.up, 90 * Time.deltaTime);
+			AdjustCannonRotation();
+		}
+
+
+		private void AdjustCannonRotation()
+		{
+			// Get the direction where the cannon has to look at:
+			// 	- If a target to attack is specified, look toward it
+			// 	- If there is no target, look forward
+			Vector3 targetDirection;
+			if (LookAtTarget == null)
+			{
+				targetDirection = transform.forward;
+			}
+			else
+			{
+				targetDirection = LookAtTarget.transform.position - CannonTurret.transform.position;
+				targetDirection.y = CannonTurret.transform.position.y;
+			}
+			// Apply the required angle to move to the desired position with a rotation delay (CannonRotateSpeed)
+			Quaternion relativeAngleTarget = Quaternion.LookRotation(targetDirection, Vector3.up);
+			CannonTurret.transform.rotation = Quaternion.RotateTowards(CannonTurret.transform.rotation, relativeAngleTarget, CannonRotateSpeed * Time.deltaTime);
+		}
+	}
+}

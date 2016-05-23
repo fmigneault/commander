b6cfdd7e5ca855fc499c0ad693deb169b78f6494
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2016-05-23 00:36:38 -0400

    Third combat units modeling and working Cannon Directional Animation for multiple types
    - Update model "Abrams" paramters
    - Udpate model "DragonLance" parameters and removed extra unrequired materials
    - Add models "IronDove", "IronMountain" and "TemperedHammer"
    - Working animation of cannon turrets for independant / combined rotation of top / bottom
    - Working animation of cannon barrels for long range angle or fixed forward targeting
    - Added debug buttons on canvas to set / remove target of all 5 models (display targeting animation systems)
    
    open issue #9, link issue #22)

--- Assets/Scripts/Units/UnitManager.cs
+++ Assets/Scripts/Units/UnitManager.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 namespace Units
 {
-	public class CombatUnitManager : MonoBehaviour 
+	public class UnitManager : MonoBehaviour 
 	{
-		public float MovingSpeed;
+		// General parameters
+		public string Name = "";
+		public bool isGroundUnit = false;
+		public bool isAirUnit = false;
 
+		// Movement parameters
+		public float MovingSpeed = 0;
+		public float RotationSpeed = 0;
+
+		// Attack parameters
+		public bool CanAttackGround = false;
+		public bool CanAttackAir = false;
+		public float MinAttackRange = 0;
+		public float MaxAttackRange = 0;
 
 		// Use this for initialization
 		void Start () 

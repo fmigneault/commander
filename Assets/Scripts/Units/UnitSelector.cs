1f4eb191c7eb3c328c07b62d30ff019293bedbeb
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2016-06-01 12:48:46 -0400

    Support unit Multi-Select vs Single Select
    - Add support for unit multi-selection using "CTRL+Left-Click" by default
    - Add support for unit multi-unselection using "CTRL+Left-Click" by default on already selected unit
    - Unit selection switched to single select as expected if only using "Left-Click" by default (ie: unselect previous units before new single select)
    - All units unselection when click on non-unit tagged gameobject
    - Add support for variable key inputs to control unit selection
    - Structure added for future "move" commands of units, function implementation to complete
    - Add OUTPUT_DEBUG define to remove logging of code easily

--- Assets/Scripts/Units/UnitSelector.cs
+++ Assets/Scripts/Units/UnitSelector.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿// Display debugging/logging info on console
+#define OUTPUT_DEBUG
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using RTS_Cam;
@@ -10,6 +13,11 @@ namespace Units
 	{
 		public string[] SelectTags;		// Tags of GameObjects which unit selection is permitted
 		public string[] AttackTags;		// Tags of GameObjects which unit attack command is permitted
+
+		// Key mappings
+		public KeyCode multipleSelectKey = KeyCode.LeftControl;
+		public KeyCode unitSelectionKey = KeyCode.Mouse0;
+		public KeyCode unitAttackMoveKey = KeyCode.Mouse1;
 
 		private float maxDistance;
 		private Camera cam;
@@ -25,35 +33,54 @@ namespace Units
 
 		void Update () 
 		{			
-			if (Input.GetMouseButtonUp(0))
-			{
-				SelectUnits();
+			Vector3 mousePosition = Input.mousePosition;
+
+			if (Input.GetKeyUp(unitSelectionKey))
+			{								
+				SelectUnits(mousePosition, Input.GetKey(multipleSelectKey));
 			}
-			else if (Input.GetMouseButtonUp(1))
+			else if (Input.GetKeyUp(unitAttackMoveKey))
 			{
-				AttackUnit();
+				bool attaking = AttackUnit(mousePosition);
+				if (!attaking) MoveUnit(mousePosition);
 			}
 		}
 
 
-		private void SelectUnits()
+		private void SelectUnits(Vector3 mousePosition, bool multipleSelect=false)
 		{
 			if (SelectTags != null)
-			{				
-				Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			{		
+				// Draw ray from camera toward pointed position, execute command only on hit (any tag)
+				Ray ray = cam.ScreenPointToRay(mousePosition);
 				RaycastHit hit;
 				if (Physics.Raycast(ray.origin, ray.direction, out hit, maxDistance))
 				{
+					// Verify if the hit gameobject tag is one of the selectable unit 
 					bool anySelected = false;
 					foreach (var tag in SelectTags)
 					{
 						if (hit.transform.CompareTag(tag) && hit.collider != null)
-						{							
+						{	
+							// Get newly pointed unit selection
 							GameObject unit = hit.collider.gameObject;
+
+							// If not using multiple selection, un-select previously selected units
+							if (!multipleSelect) UnSelectAllUnits();						
+
+							// If using multiple selection, un-select already selected unit
+							if (multipleSelect && selectedUnits.Contains(unit))
+							{
+								UnselectSingleUnit(unit);
+								return;
+							}
+
+							// Update unit selection
 							SetUnitHighlight(unit);
 							selectedUnits.Add(unit);
-							anySelected = true;
+							anySelected = (selectedUnits.Count > 0);
 
+							#if OUTPUT_DEBUG
 							#region DEBUG
 							if (unit != null && unit.GetComponent<UnitManager>() != null) 
 							{
@@ -61,22 +88,31 @@ namespace Units
 								Debug.Log(string.Format("Selected: {0}, {1}", tag, hitName));
 							}
 							#endregion
+							#endif
 						}
 
-						if (!anySelected) UnSelectUnits();
+						// If no valid unit tag was hit, unselect units (ex: click on terrain)
+						if (!anySelected) UnSelectAllUnits();
 					}					
 				}	
 			}
 		}
 
 	
-		private void UnSelectUnits()
+		private void UnSelectAllUnits()
 		{
 			foreach (var unit in selectedUnits)
 			{
 				ResetUnitHighlight(unit);
 			}
 			selectedUnits.Clear();
+		}
+
+
+		private void UnselectSingleUnit(GameObject unit) 
+		{			
+			selectedUnits.Remove(unit);
+			ResetUnitHighlight(unit);
 		}
 
 
@@ -92,25 +128,29 @@ namespace Units
 		}
 
 
-		private void AttackUnit() 
+		private bool AttackUnit(Vector3 mousePosition) 
 		{
 			if (AttackTags != null && selectedUnits.Count != 0)
-			{				
-				Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			{			
+				// Draw ray from camera toward pointed position, execute command only on hit (any tag)	
+				Ray ray = cam.ScreenPointToRay(mousePosition);
 				RaycastHit hit;
 				if (Physics.Raycast(ray.origin, ray.direction, out hit, maxDistance))
 				{
+					// Verify if the hit gameobject tag is one that can be attacked
 					bool anyAttacked = false;
 					foreach (var tag in AttackTags)
 					{
 						if (hit.transform.CompareTag(tag) && hit.collider != null)
-						{								
+						{			
+							// Apply newly pointed unit as target of already selected units
 							GameObject target = hit.collider.gameObject;
 							foreach (var unit in selectedUnits)
 							{
 								// Ask for the attack command if the selected/attacked units are not the same (cannot attack itself)
 								if (target != unit) unit.GetComponent<UnitManager>().Attack(target);
 
+								#if OUTPUT_DEBUG
 								#region DEBUG
 								if (unit != null && unit.GetComponent<UnitManager>() != null && target != null && target.GetComponent<UnitManager>() != null) 
 								{
@@ -119,10 +159,13 @@ namespace Units
 									Debug.Log(string.Format("Attack: {0}, {1} => {2}, {3}", unit.tag, attackUnitName, tag, hitName));
 								}
 								#endregion
+								#endif
 							}
 							anyAttacked = true;
 						}
 					}	
+
+					// If no unit was hit for an attack, cancel any existing attack command by removing the target reference
 					if (!anyAttacked)
 					{
 						foreach (var unit in selectedUnits)
@@ -130,8 +173,20 @@ namespace Units
 							unit.GetComponent<UnitManager>().Attack(null);
 						}
 					}
+					else return true;	// True if any unit was attacked
 				}	
 			}
+			return false;	// False in any case except when any unit was attacked
+		}
+
+
+		private bool MoveUnit(Vector3 mousePosition)
+		{
+
+
+
+
+			return false;	// Return value in case of invalid movement
 		}
 	}
 }

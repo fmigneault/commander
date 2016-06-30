f752ab5368411794e8177d7f058dc866a2c97e4c
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2016-06-30 04:15:42 -0400

    Minimap Movement implemented  (ref issue #5)
    - Working Minimap movement, but fix required
       * Movement now done properly when the map has an aspect ratio of 1:1, but different ratios doesn't adjust properly the position (need to multiply lower size by 1/ratio?)
       * Using minimap pixel position on screen and input mouse in pixels, it should therefore not affected by any minimap repositioning
       * Still left most of the commented code (TO BE REMOVED)
    
    -  Improvements
       * Still scrolls when close to border, could be disabled when OnMouseOver (ref issue #39)
       * Possibility to add backward offset (with pythagore if not out of map bounds?) to view the requested position with the camera, instead of being on top of it (ref issue #40)
       * Mouse dragging on minimap for consecutive quick movements (ref issue #41)
       * Possibility to display the area viewed by the camera using a bounding box (square / trapezoid) (ref issue #42)
    
     - Fixed Bug
       * Minimap properly displayed over backgrounds (UI & Minimap BG) (resolve issue #38)
       * Changed order of elements and also modified canvas BG to include the image disrectly in the canvas object

--- Assets/Scripts/Cameras/MiniMapManager.cs
+++ Assets/Scripts/Cameras/MiniMapManager.cs
@@ -4,14 +4,15 @@ using UnityEngine.UI;
 
 namespace Cameras
 {
-    /////////[RequireComponent(typeof(Camera))]
     public class MiniMapManager : MonoBehaviour 
     {        
         public Terrain GroundTerrain;
+        public RectTransform MinimapPosition;
 
         ////////public RectTransform MiniMapPanelArea;
 
-        private Camera minimap;
+        public Camera CameraMiniMap;
+        public Camera CameraRTS;
         /////private Rect backgroundArea;
         //////private float MapLeftBorder = 0.032f;
         /////private float MapTopBorder = 0.01f;
@@ -20,7 +21,7 @@ namespace Cameras
 
     	void Start() 
         {
-            minimap = GetComponent<Camera>();
+            
             //////backgroundArea = GetComponentInParent<Canvas>().pixelRect;           
     	}
 
@@ -31,18 +32,102 @@ namespace Cameras
         }
 
 
+        // Function called only when a click is detected on the minimap
+        // Gets the requested position and moves the carema to the corresponding terrain position
+        // Interpolation is done using relative coordinates, so moving/resizing the minimap would not affect the process
         public void OnMiniMapClick()
-        {
-            Debug.Log("OnMiniMapClick FIRED!");
+        {            
+//            Debug.Log(GetComponentInParent<Canvas>().GetComponent<RectTransform>().rect);
+//            Debug.Log(MinimapPosition.position);
+//            Debug.Log(Input.mousePosition);
+//
+//            var corners = new Vector3[4];
+//            MinimapPosition.GetWorldCorners(corners);
+//            Debug.Log(string.Format("C1: ({0}), C2: ({1}), C3: ({2}), C4: ({3})",
+//                corners[0], corners[1], corners[2], corners[3]));
+//
+//
+//
+            // Get pointed mouse position in pixels, (0,0) left-bottom of visible screen
+            var minimapPointed = Input.mousePosition;
 
-            // Get pointed mouse position normalized for MiniMap area
-            var pointed = Input.mousePosition;
-            Debug.Log(string.Format("Pointed {0}, Norm: {1}", pointed, pointed.normalized));
+            // Get the minimap corner positions in pixels, relative to (0,0) left-bottom of visible screen
+            var minimapCorners = new Vector3[4];
+            MinimapPosition.GetWorldCorners(minimapCorners);           
+
+            // Obtain key positions of minimap
+            var xMinMiniMap = minimapCorners[0].x;
+            var xMaxMiniMap = minimapCorners[2].x;
+            var yMinMiniMap = minimapCorners[0].y;
+            var yMaxMiniMap = minimapCorners[2].y;
+
+            // Obtain terrain dimensions as 2D plane
+            var wTerrain = GroundTerrain.terrainData.size.x;
+            var hTerrain = GroundTerrain.terrainData.size.z;
+
+            // Transfer pixel positions to terrain positions, centered at (0,0,0)
+            var xTerrain = (minimapPointed.x - xMinMiniMap) / (xMaxMiniMap - xMinMiniMap) * wTerrain - wTerrain / 2;
+            var yTerrain = CameraRTS.transform.position.y;
+            var zTerrain = (minimapPointed.y - yMinMiniMap) / (yMaxMiniMap - yMinMiniMap) * hTerrain - hTerrain / 2;
+
+            // Move to new terrain position (camera movement relative to centered terrain, terrain doesn't move)
+            CameraRTS.transform.position = new Vector3(xTerrain, yTerrain, zTerrain);
+
+
+            Debug.Log(minimapPointed);
+            Debug.Log(xMinMiniMap);
+            Debug.Log(yMinMiniMap);
+            Debug.Log(xMaxMiniMap);
+            Debug.Log(yMaxMiniMap);
+            Debug.Log(wTerrain);
+            Debug.Log(hTerrain);
+            Debug.Log(xTerrain);
+            Debug.Log(yTerrain);
+            Debug.Log(zTerrain);
+        }
+
+//            // Get pointed mouse position normalized for MiniMap area
+//            var minimapPointed = Input.mousePosition.normalized;
+//
+//            // Get the central position of the minimap that corresponds to terrain position XZ(0,0)
+//            // (true since the pivot position is set at (0.5,0.5) of the whole minimap anchor points)
+//            var minimapCenter = MinimapPosition.position.normalized;
+//
+//            // Get the new position relatively to the center (±offset)
+//            var minimapRelativePosition = minimapPointed - minimapCenter;
+//
+//            // Convert relative position to terrain centered dimensions (2D -> 3D, using camera height)
+//            var terrainRelativePosition = new Vector3(minimapRelativePosition.x * GroundTerrain.terrainData.size.x / 2, 
+//                                                      CameraRTS.transform.position.y,
+//                                                      minimapRelativePosition.y * GroundTerrain.terrainData.size.z / 2);
+
+//            Debug.Log(string.Format("Minimap Point: ({0},{1}), Minimap Center: ({2},{3}), Minimap Rel: ({4},{5}), Terrain: ({6},{7},{8})",
+//                minimapPointed.x, minimapPointed.y, minimapCenter.x, minimapCenter.y,
+//                minimapRelativePosition.x, minimapRelativePosition.y, 
+//                terrainRelativePosition.x, terrainRelativePosition.y, terrainRelativePosition.z));
+
+
+//            // Move to new terrain position (camera movement relative to centered terrain, terrain doesn't move)
+//            CameraRTS.transform.position = terrainRelativePosition;
+
+
+//            Debug.Log(string.Format("Pos: ({0},{1}), Anchor Pos: ({2},{3}), min: ({4},{5}), max: ({6},{7}), W/H: ({8},{9})",
+//                MinimapPosition.position.x, MinimapPosition.position.y, 
+//                MinimapPosition.anchoredPosition.normalized.x, MinimapPosition.anchoredPosition.normalized.y, 
+//                MinimapPosition.rect.xMin, MinimapPosition.rect.yMin, 
+//                MinimapPosition.rect.xMax, MinimapPosition.rect.yMax,
+//                MinimapPosition.rect.xMax - MinimapPosition.rect.xMin, 
+//                MinimapPosition.rect.yMax - MinimapPosition.rect.yMin));
+//
+//            var corners = new Vector3[4];
+//            MinimapPosition.GetWorldCorners(corners);
+//            Debug.Log(string.Format("C1: ({0}), C2: ({1}), C3: ({2}), C4: ({3})",
+//                corners[0], corners[1], corners[2], corners[3]));
 
 
             // SEE ||||||| Rect.PointToNomalized  ||   Rect.NormalizedToPoint
 
-        }
+       // }
 
 
         // Adjusts the camera dimension settings to properly position it where the MiniMapPanelArea is specified
@@ -58,9 +143,9 @@ namespace Cameras
 //            minimap.rect = new Rect(MiniMapPanelArea.anchorMin.x, MiniMapPanelArea.anchorMin.y, anchorWidth, anchorHeight);
 
             // Apply terrain dimension to orthographic half-size to capture all of it on the map 
-            if (minimap != null)
+            if (CameraMiniMap != null)
             {
-                minimap.orthographicSize = Mathf.Max(GroundTerrain.terrainData.size.x, GroundTerrain.terrainData.size.z) / 2;
+                CameraMiniMap.orthographicSize = Mathf.Max(GroundTerrain.terrainData.size.x, GroundTerrain.terrainData.size.z) / 2;
             }
         }
 
